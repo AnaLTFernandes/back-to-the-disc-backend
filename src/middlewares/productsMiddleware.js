7ee1b01b2e4a61f97628c093e01a994a3652c4cc
Ana Fernandes <analuizateixeirafernandes184@gmail.com>
2022-11-29 15:00:03 -0300

    fix: page validation

--- src/middlewares/productsMiddleware.js
+++ src/middlewares/productsMiddleware.js
@@ -1,9 +1,9 @@
 import { STATUS_CODE } from "../enums/statusCode.js";
 
 function hasPageMiddleware (req, res, next) {
-    const page = Number(req.query.page) || null;
+    const { page } = req.query;
 
-    if (!page || page < 1) return res.sendStatus(STATUS_CODE.UNPROCESSABLE_ENTITY);
+    if (!page || isNaN(page) || page < 1) return res.sendStatus(STATUS_CODE.UNPROCESSABLE_ENTITY);
 
     next();
 }
